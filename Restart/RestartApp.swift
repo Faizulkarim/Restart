63e5ed354c62565319f6bcaa06334f10e7fe5e5d
Md Faizul karim <faizulkarim28@gmail.com>
2024-07-04 01:57:58 +0600

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Md Faizul karim on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
