c32b5126c19f1fd2f961793cf2ab3960969f4c1b
Md Faizul karim <faizulkarim28@gmail.com>
2024-07-11 21:28:06 +0600

    Added color extension , Background of homeview

---
+++ Restart/Utility/ColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtension.swift
+//  Restart
+//
+//  Created by Md Faizul karim on 11/7/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let customRed = Color("ColorRed")
+    static let customBlue = Color("ColorBlue")
+}
