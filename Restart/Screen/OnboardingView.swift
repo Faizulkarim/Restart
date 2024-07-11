08ac8fb6b155801fbe4ef909f91c4cab8cee8abb
Md Faizul karim <faizulkarim28@gmail.com>
2024-07-12 00:42:25 +0600

    added gester for centaral image and added dynamic title

--- Restart/Screen/OnboardingView.swift
+++ Restart/Screen/OnboardingView.swift
@@ -14,6 +14,7 @@ struct OnboardingView: View {
     @State private var buttonWidht: Double = (UIScreen.current?.bounds.width)! - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var indicatorOpactiy = 1.0
+    @State private var textTitle = "Share."
     let hapticFeedBack = UINotificationFeedbackGenerator()
     
     var body: some View {
@@ -26,7 +27,7 @@ struct OnboardingView: View {
                 Spacer()
                 
                 VStack {
-                    Text("Share")
+                    Text(textTitle)
                         .font(.system(size: 60))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
@@ -53,6 +54,30 @@ struct OnboardingView: View {
                         .animation(.easeInOut(duration: 0.5), value: isAnimating)
                         .offset(x: imageOffset.width * 1.2 , y: 0)
                         .rotationEffect(.degrees(imageOffset.width / 20))
+                        .gesture(
+                           DragGesture()
+                            .onChanged({ gester in
+                                if abs(imageOffset.width) <= 150 {
+                                    imageOffset = gester.translation
+                                    
+                                }
+                                
+                                withAnimation(.linear(duration: 0.25)) {
+                                    indicatorOpactiy = 0
+                                    textTitle = "Give"
+                                }
+                                
+                            })
+                            .onEnded({ _ in
+                                imageOffset = .zero
+                                
+                                withAnimation(.linear(duration: 0.25)) {
+                                    indicatorOpactiy = 1
+                                    textTitle = "Share."
+                                }
+                            })
+                        )
+                        .animation(.easeOut(duration: 1), value: imageOffset )
                 }
                 .overlay(
                     Image(systemName: "arrow.left.and.right.circle")
